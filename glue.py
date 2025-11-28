1eb8d6e5e45a193355f72c7ba375ee983b8b7d98
eeqi98 <lim.ee@northeastern.edu>
2025-11-27 19:51:26 -0800

    Add CloudWatch metric logging

--- glue.py
+++ glue.py
@@ -3,7 +3,28 @@ from pyspark.sql.functions import col, lower, regexp_replace, trim, length
 from pyspark.ml.feature import Tokenizer, StopWordsRemover, CountVectorizer, IDF
 from awsglue.context import GlueContext
 from awsglue.job import Job
+import boto3
+import logging
+import time
 
+cloudwatch = boto3.client("cloudwatch")
+logger = logging.getLogger("glue")
+logger.setLevel(logging.INFO)
+
+
+def cloudwatch_metric(metric, value, unit="Count"):
+    cloudwatch.put_metric_data(
+        Namespace="Glue",
+        MetricData=[{
+            "MetricName": metric,
+            "Value": float(value),
+            "Unit": unit
+        }]
+    )
+    logger.info(f"{metric}: {value}")
+
+
+start_time = time.time()
 sc = SparkContext()
 glueContext = GlueContext(sc)
 spark = glueContext.spark_session
@@ -16,12 +37,18 @@ output_path = "s3://pj-cs6240/output/"
 # Load raw reviews
 df = spark.read.json(input_path)
 
+initial_count = df.count()
+cloudwatch_metric("Initial_Rows_Count", initial_count)
+
 # Filter nulls and verified reviews
 df_clean = df.filter(
     (col("reviewText").isNotNull()) &
     (col("summary").isNotNull()) &
     (col("verified") == True)
 )
+
+filtered_count = df_clean.count()
+cloudwatch_metric("Filtered_Rows_Count", filtered_count)
 
 # Normalize text and overwrite original columns
 df_clean = df_clean.withColumn("reviewText",
@@ -38,9 +65,13 @@ df_tokens = tokenizer.transform(df_clean)
 remover = StopWordsRemover(inputCol="tokens", outputCol="filtered_tokens")
 df_tokens = remover.transform(df_tokens)
 
-# TF-IDF
+# Term Frequency – Inverse Document Frequency (TF-IDF) Feature Extraction
 cv = CountVectorizer(inputCol="filtered_tokens", outputCol="raw_features", vocabSize=1000)
 cv_model = cv.fit(df_tokens)
+
+vocab_size = len(cv_model.vocabulary)
+cloudwatch_metric("TFIDF_Vocab_Size", vocab_size)
+
 df_cv = cv_model.transform(df_tokens)
 idf = IDF(inputCol="raw_features", outputCol="tfidf_features")
 idf_model = idf.fit(df_cv)
@@ -49,5 +80,9 @@ df_final = idf_model.transform(df_cv)
 # Save cleaned review text and features
 df_final.select("reviewerID", "asin", "overall", "verified", "review_length", "summary_length", "reviewText",
                 "tfidf_features").write.mode("overwrite").parquet(output_path)
+
+# Runtime Logging
+runtime = round(time.time() - start_time, 2)
+cloudwatch_metric("Total_Runtime", runtime, unit="Seconds")
 
 job.commit()
